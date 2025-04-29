904e40dd3b9da69b029e638913de892ed52ddac3
ganesh <ganeshbhanu3231@gmail.com>
2025-04-29 05:59:29 +0000

    Moved all files to a folder and committing it to testBranch

---
+++ shellPracticeScripts/calculator.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+x =a+b
+
+#for new branch
+y = a-b
---
+++ shellPracticeScripts/createFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#FIrst create a folder
+mkdir scriptCreatedFolder
+#Now change directory to create the folder there
+cd scriptCreatedFolder
+#Now create the files
+touch firstFile.txt secondFile.txt
